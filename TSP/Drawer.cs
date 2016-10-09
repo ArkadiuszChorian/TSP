1943c894206c4f4b701281c13b8ff3e7ad527f13
patrykk <patrykk.93@gmail.com>
2016-10-09 16:19:22 +0200

    Completed drawer. Draws 800x400 bitmap with red Points as circle and blue lines as Path.

--- TSP/Drawer.cs
+++ TSP/Drawer.cs
@@ -9,35 +9,86 @@ namespace TSP
 {
     class Drawer
     {
-        public void DrawChart(String filename, List<Node> fullNodes, List<Node> path)
+        /// <summary>
+        /// Creates bitmap based on all points and path taken by algorithm. 
+        /// Method saves bitmap to file in ".../TSP/bin/Debug/<param name="filename"/>       
+        /// </summary>  
+        public void DrawChart( String filename, List<Node> fullNodes, List<Node> path )
         {
             Bitmap cpy;
             try
             {
-                Bitmap bmp = new Bitmap(800, 800);
+                // 5px margin
+                Bitmap bmp = new Bitmap(805, 405);
                 cpy = (Bitmap)bmp.Clone();
                 bmp.Dispose();
 
-                using (Graphics g = Graphics.FromImage(cpy))
+                using ( Graphics g = Graphics.FromImage(cpy) )
                 {
                     g.Clear(Color.White);
                     //g.DrawLine(Pens.Red, 50, 20, 50, 40);
-                    for (int i = 0; i < fullNodes.Count; i++)
+                    for ( int i = 0; i < fullNodes.Count; i++ )
                     {
-                        g.DrawEllipse(Pens.Red,
-                            new RectangleF(new PointF(fullNodes[i].X, fullNodes[i].Y), new SizeF(5.0f, 5.0f)));
+                        Coordinate coords = prepareCoordinates(fullNodes[i].X, fullNodes[i].Y);
+                        g.FillEllipse(Brushes.Red,
+                            new RectangleF(new PointF(coords.X, coords.Y), new SizeF(6.0f, 6.0f)));
+                    }
+
+                    Coordinate previous = new Coordinate();
+                    bool firstTime = true;
+
+                    for (int i = 0; i < path.Count; i++ )
+                    {
+                        Coordinate coords = prepareCoordinates(path[i].X, path[i].Y);
+
+                        if ( firstTime )
+                        {
+                            previous = coords;
+                            firstTime = false;
+                            continue;
+                        }
+
+                        g.DrawLine(Pens.Blue, 
+                            new PointF(previous.X, previous.Y), 
+                            new PointF(coords.X, coords.Y));
+
+                        previous = coords;
                     }
                 }
+
+                if ( System.IO.File.Exists(filename) )
+                    System.IO.File.Delete(filename);
 
                 cpy.Save(filename);
                 cpy.Dispose();
             }
-            catch (Exception ex)
+            catch ( Exception ex )
             {
                 Console.WriteLine("Conversion failed: {0}", ex.Message);
             }
-            //cpy.SetResolution(96, 96);
+            //
 
         }
+
+        /// <summary>
+        /// Scaling coordinates for bitmap with factor = 5
+        ///(max X value = 4000, scaled = 800, Y value 2000, scaled = 400)
+        /// </summary>
+        private Coordinate prepareCoordinates( int x, int y )
+        {
+            Coordinate c = new Coordinate();
+            c.X = (float)Math.Round((double)x / 5.0);
+            c.Y = (float)( Math.Round((double)y / 5.0) );
+            return c;
+        }
+    }
+
+    /// <summary>
+    /// Class to keep scaled coordinates
+    /// </summary>
+    class Coordinate
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
     }
 }
